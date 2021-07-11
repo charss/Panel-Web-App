6807fabd61ff28c84827d06b93965eb6c066ef20
charss <charleskylereyes16@gmail.com>
2021-07-11 14:21:06 +0800

    Minor patches

--- website/models.py
+++ website/models.py
@@ -2,7 +2,7 @@ from . import db
 # from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
