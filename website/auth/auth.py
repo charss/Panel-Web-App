b7e44fe09678353c24f375c3a2ed2c78137cab35
charss <charleskylereyes16@gmail.com>
2021-07-11 13:13:38 +0800

    Working login

--- website/auth/auth.py
+++ website/auth/auth.py
@@ -1,50 +1,69 @@
-from flask import Blueprint, redirect, url_for, render_template, request, session, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from ..models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from .. import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__, static_folder='static', template_folder='templates')
 
 
-
-@auth.route('/', methods=['POST', 'GET'])
-def home():
+@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        session.permanent = True
-        user = request.form['username']
-        password = request.form['pass']
-
-        if user == 'admin' and password == 'admin':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email == 'admin' and password == 'admin':
             return redirect(url_for('admin.home'))
-    else:
-        return render_template('login.html')
+
+        # user = User.query.filter_by(email=email).first()
+        # if user:
+        #     if check_password_hash(user.password, password):
+        #         flash('Logged in successfully!', category='success')
+        #         login_user(user, remember=True)
+        #         return redirect(url_for('views.home'))
+        #     else:
+        #         flash('Incorrect password, try again.', category='error')
+        # else:
+        #     flash('Email does not exist.', category='error')
+
+    return render_template("login.html")
 
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
 
-# @login.route("", methods=['POST', 'GET'])
-# def login():
-#     if request.method == 'POST':
-#         session.permanent = True
-#         user = request.form['nm']
-#         session['user'] = user
 
-#         # Check if exist in database
-#         found_user = users.query.filter_by(name=user).first()
-#         # Delete an object
-#         # found_user = users.query.filter_by(name=user).first()
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
 
-#         # If there are many objects
-#         # for user in found_user:
-#         #     user.delete()
-#         if found_user:
-#             session['email'] = found_user.email
-#         else:
-#             usr = users(user, '')
-#             db.session.add(usr)
-#             db.session.commit()
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
-#         flash(f'Login Successful!', "info")
-#         return redirect(url_for("user"))
-#     else:
-#         if 'user' in session:
-#             flash(f'Already Logged In!', "info")
-#             return redirect(url_for("user"))
-
-#         return render_template("login.html")
+    return render_template("sign_up.html", user=current_user)
