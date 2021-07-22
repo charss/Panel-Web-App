8c1801fca9b087a9df7ca68bc247ee5d98782728
charss <charleskylereyes16@gmail.com>
2021-07-22 20:33:46 +0800

    Update user grading system

--- website/user.py
+++ website/user.py
@@ -4,7 +4,6 @@ from flask_login import login_required, current_user
 from .models import Group, Student, Panelist, Defense
 from . import db
 from .populate import *
-from datetime import datetime, date, time, timedelta
 
 
 user = Blueprint('user', __name__, static_folder='static', template_folder='templates/user')
@@ -29,3 +28,18 @@ def user_panelists():
 @login_required
 def user_students():
     return render_template('students_u.html', user=current_user, students=Student.query.all())
+
+@user.route("/grading/<tab>/<content>")
+@login_required
+def indiv_sheet(tab, content):
+    defense = db.session.query(Defense).filter_by(id=content).first()
+    if tab == 'indiv':
+        return render_template('grading/indiv_sheet.html', defense=defense)
+    elif tab == 'group':
+        return render_template('grading/group_sheet.html', defense=defense)
+    elif tab == 'comment':
+        return render_template('grading/comment_sheet.html', defense=defense)
+
+
+
+
